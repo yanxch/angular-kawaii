ec4350dbea87fce9e600f72f712666eb91247ece
Christian Janker <janker.christ@gmail.com>
2019-11-01 18:01:40 +0100

    WIP - Planet

---
+++ projects/sample-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PlanetComponent } from './planet/planet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlanetComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
